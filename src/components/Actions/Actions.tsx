fb0dd163516e898bd5745ab3dda83cba0e2a9e99
Cenk SARI <cenk@cenksari.com>
2024-05-08 10:01:57 +0100

    actions links

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -12,15 +12,15 @@ const Actions = (): React.JSX.Element => {
         <span className='text-shadow'>Add money</span>
       </div>
       <div className='circle no-select flex flex-col flex-v-center flex-h-center'>
-        <button type='button' className='flex flex-v-center flex-h-center'>
+        <Link to='/home' className='flex flex-v-center flex-h-center'>
           <span className='material-symbols-outlined'>sync</span>
-        </button>
+        </Link>
         <span className='text-shadow'>Exchange</span>
       </div>
       <div className='circle no-select flex flex-col flex-v-center flex-h-center'>
-        <button type='button' className='flex flex-v-center flex-h-center'>
+        <Link to='/home' className='flex flex-v-center flex-h-center'>
           <span className='material-symbols-outlined'>page_info</span>
-        </button>
+        </Link>
         <span className='text-shadow'>Details</span>
       </div>
       <div className='circle no-select flex flex-col flex-v-center flex-h-center'>
